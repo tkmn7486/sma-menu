682a3139b1fefc4f532e22b9d791218819cac3ad
tkmn7486 <71046924+tkmn7486@users.noreply.github.com>
2022-03-07 11:41:53 +0900

    add microCMS

---
+++ pages/_slug/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ model_name }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://sma-tools.microcms.io/api/v1/iphone-repair/${params.slug}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': 'c40a31162fe14cca8714a9dd34fc021c0438' }
+      }
+    )
+    return data
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <Tutorial />
+  <div class="app">
+    <h2>iPhone修理料金</h2>
+    <div class="contents">
+      <ul>
+        <li v-for="content in contents" :key="content.id">
+          <nuxt-link :to="`/${content.id}`">
+            {{ content.model_name }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'IndexPage',
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://sma-tools.microcms.io/api/v1/iphone-repair',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'c40a31162fe14cca8714a9dd34fc021c0438' }
+      }
+    )
+    return data
+  }
 }
 </script>
+<style>
+  h2{
+    font-size:30px;
+  }
+</style>
